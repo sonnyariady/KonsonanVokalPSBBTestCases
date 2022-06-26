e7631b52abf1d0d5d3b5c20309580d7e2b75b119
Sonny Ariady <sonnyariady@gmail.com>
2022-06-26 07:15:56 +0700

    first commit

---
+++ KonsonanVokal/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace KonsonanVokal
+{
+    class Program
+    {
+		public static string procVowel(string param)
+		{
+			string vowels = "aeiou";
+			param = param.Replace(" ", string.Empty).ToLower();
+			string result = new string(param.Where(c => vowels.Contains(c)).ToArray());
+
+			Dictionary<char, int> valuePairs = new Dictionary<char, int>();
+
+			foreach (var item in result)
+            {
+				if (!valuePairs.ContainsKey(item))
+                {
+					valuePairs.Add(item, 1);
+                }
+				else
+                {
+					valuePairs[item] = valuePairs[item] + 1;
+
+				}
+            }
+
+			List<char> lstUrut = new List<char>();
+
+			foreach (KeyValuePair<char, int> entry in valuePairs)
+			{
+				// do something with entry.Value or entry.Key
+				int total = entry.Value;
+				for (int i = 0; i < total; i++)
+					lstUrut.Add(entry.Key);
+			}
+			
+			return new string(lstUrut.ToArray());
+		}
+
+		public static string procConsonant(string param)
+		{
+			string vowels = "aeiou";
+			param = param.Replace(" ", string.Empty).ToLower();
+			string result = new string(param.Where(c => !vowels.Contains(c)).ToArray());
+
+			Dictionary<char, int> valuePairs = new Dictionary<char, int>();
+
+			foreach (var item in result)
+			{
+				if (!valuePairs.ContainsKey(item))
+				{
+					valuePairs.Add(item, 1);
+				}
+				else
+				{
+					valuePairs[item] = valuePairs[item] + 1;
+
+				}
+			}
+
+			List<char> lstUrut = new List<char>();
+
+			foreach (KeyValuePair<char, int> entry in valuePairs)
+			{
+				// do something with entry.Value or entry.Key
+				int total = entry.Value;
+				for (int i = 0; i < total; i++)
+					lstUrut.Add(entry.Key);
+			}
+
+			return new string(lstUrut.ToArray());
+		}
+
+		public static void Main()
+		{
+			Console.Write("Input one line of words (S) : ");
+			string input = Console.ReadLine();
+
+			string charVowel = procVowel(input);
+			string charConsonant = procConsonant(input);
+
+			Console.WriteLine("Vowel Characters : ");
+			Console.WriteLine(charVowel);
+			Console.WriteLine("Consonant Characters : ");
+			Console.WriteLine(charConsonant);
+			Console.Write("Press any key to continue...");
+			Console.ReadKey();
+		}
+	}
+}
---
+++ PSBB/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PSBB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input the number of family: ");
+            int jumlah = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Input the number of members in the family (separated by space): ");
+            string members = Console.ReadLine();
+            List<int> lstMember = members.Split(' ').Select(a=> Convert.ToInt32(a)).ToList();
+
+            if (jumlah != lstMember.Count())
+            {
+                Console.WriteLine("Input must be equal with the count of family");
+            }
+            else
+            {
+                int totalorang = lstMember.Sum(a => a);
+                double minbus = totalorang / (double)4;
+                Console.WriteLine("Minimum bus required is " + Math.Ceiling(minbus));
+            }
+
+            Console.Write("Press any key to continue...");
+            Console.ReadKey();
+        }
+    }
+}
